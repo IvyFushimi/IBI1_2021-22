f1d0f223dcb7f1d3c809da2e5e22e66062b60207
IvyFushimi <101172418+IvyFushimi@users.noreply.github.com>
2022-05-23 15:50:17 +0800

    Update chd.py

--- Practical6/chd.py
+++ Practical6/chd.py
@@ -12,6 +12,7 @@ dict = {'paternal_age:chd':{
     '70':'1.72',
     '75':'1.94'
 }}
+dict2 = dict['paternal_age:chd']
 #print the dict
 print('paternal_age:chd=',dict['paternal_age:chd'])
 #print the age and get the return of the value of the risk
@@ -21,6 +22,6 @@ print(dict[age])
 #get the graph of the scatter
 import matplotlib.pyplot as plt#import the library of graphing
 x = 30,35,40,45,50,55,60,65,70,75
-y = dict['30'], dict['35'], dict['40'], dict['45'], dict['50'], dict['55'], dict['60'], dict['65'], dict['70'], dict['75']
+y = dict2.values()
 plt.scatter(x,y,marker='o')
 plt.show()
